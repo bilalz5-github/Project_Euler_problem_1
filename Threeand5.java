14db651a4bfa3a7ff50fec384c6d6e53753d590e
Bilalz <Bilalazam1678@gmail.com>
2023-06-17 19:47:08 +0300

    these are two files of java in which i solved the problem no 1 of project euler

---
+++ Threeand5.java
@@ -0,0 +1,7 @@
+
+public class Threeand5 {
+
+	public static void main(String[] args) {
+		
+		double Num1 = 600851475143;
+}
---
+++ function.java
@@ -0,0 +1,23 @@
+
+public class function {
+
+	
+	 int Num1 = 0;
+     int Num2 = 1;
+     int sum = 0;
+     {
+
+     do
+     {
+         sum = Num1 + Num2;
+         Num1 = Num2;
+         Num2 = sum;
+
+         if (Num2 % 2 == 0)
+             sum = sum + Num2;
+     }
+     while (Num2 < 4000000);
+
+     System.out.println(sum);
+	}
+}
